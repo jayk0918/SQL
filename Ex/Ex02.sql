7069fe9395748a4e603513eee4341d1f7ba8f736
Mocking_jayk0918 <jesper2able@gmail.com>
2022-05-12 17:23:47 +0900

    1.0(lecture_gittestAtWindows 22.05.12 17:23)

--- Ex/Ex02.sql
+++ Ex/Ex02.sql
@@ -120,9 +120,272 @@ where hire_date > '07/01/01';
 
 -- 이름이 Lex인 직원의 연봉을 출력하세요
 select salary
+-- 끝내기 전에 이름이 Lex가 맞는지 first_name도 출력해 볼 것
 from employees
 where first_name = 'Lex';
 
+-- 조건이 2개 이상일 경우
+-- 예제) 연봉이 14000이상 17000이하인 사원의 이름과 연봉을 구하시오 (and 활용)
+
+select  first_name
+        ,salary
+from employees
+where salary >= 14000
+and salary <= 17000;
+
+-- 연봉이 14000 이하이거나 17000 이상인 사원의 이름과 연봉을 출력하세요 (or 활용)
+select  first_name
+        ,salary
+from employees
+where salary <= 14000
+or salary >= 17000;
+-- null;
+
+-- 입사일이 04/01/01에서 05/12/31 사이의 사원의 이름과 입사일을 출력하세요
+select  first_name
+        ,hire_date
+from employees
+where hire_date >= '04/01/01'
+and hire_date <= '05/12/31';
+
+-- between 연산자 활용(특정구간 값 출력, 이상&이하 개념)
+-- 처리 속도가 느린 편 / 처리할 데이터의 양이 많아질 경우 이슈가 될 수 있음
+select  first_name
+        ,salary
+from employees
+where salary between 14000 and 17000;
+
+-- in 연산자 활용 (여러 조건 검사)
+select first_name, last_name, salary
+from employees
+where first_name in ('Neena', 'Lex', 'John');
+
+/*
+where first_name = 'Neena'
+or first_name = 'Lex'
+or first_nam = 'John';  
+*/
+
+-- 예제)
+-- 연봉이 2100, 3100, 4100, 5100 인 사원의 이름과 연봉을 구하시오
+
+select  first_name
+        ,salary
+from employees
+where salary in (2100, 3100, 4100, 5100);
+
+select  first_name
+        ,salary
+from employees
+where salary = 2100
+or salary = 3100
+or salary = 4100
+or salary = 5100;
+
+-- like 연산자
+select  first_name
+        ,last_name
+        ,salary
+from employees
+where first_name like 'L%';
+-- 해석 : like(~와 같은) + first_name에 'L'이라는 글자가 포함된 모든 결과값 출력
+-- 'L'뒤에 %는 무엇이 오든 상관이 없다는 의미 (공백도 가능)
+
+select  first_name
+        ,last_name
+        ,salary
+from employees
+where first_name like 'L___';
+-- 'L'뒤에 _(underbar)는 L뒤에 오는 글자개수를 특정짓는 기호 (위에는 3개가 설정됨)
+
+select  first_name
+        ,last_name
+        ,salary
+from employees
+where first_name like '%s%';
+-- first_name에 s가 존재하면 모두 출력
+
+-- [예제]
+-- 이름에 am 을포함한 사원의 이름과 연봉을 출력하세요
+
+select  first_name
+        ,salary
+from employees
+where first_name like '%am%';
+
+-- 이름의 두번째 글자가 a인 사원의 이름과 연봉을 출력하세요
+
+select  first_name
+        ,salary
+from employees
+where first_name like '_a%';
+
+-- 이름의 네번째 글자가 a인 사원의 이름을 출력하세요
+
+select first_name
+from employees
+where first_name like '___a%';
+
+-- 이름이 4글자인 사원중 끝에서 두번째 글자가 a인 사원의 이름을 출력하세요
+
+select first_name
+from employees
+where first_name like '__a_';
+
+-- null(값이 부재) vs 0 (값이 존재)
+select  first_name
+        ,salary
+        ,commission_pct
+        ,salary * commission_pct
+from employees
+where salary between 13000 and 15000;
+
+-- is null / is not null
+select  first_name
+        ,salary
+        ,commission_pct
+        ,salary * commission_pct
+from employees
+where commission_pct is null;
+-- where commission_pct = null; 이라고 할 경우 ㄹㅇ로 null이라는 값을 찾음
+
+select  first_name
+        ,salary
+        ,commission_pct
+        ,salary * commission_pct
+from employees
+where commission_pct is not null;
+
+
+-- [예제]
+-- 커미션 비율이 있는 사원의 이름과 연봉 커미션 비율을 출력하세요
+
+select  first_name
+        ,commission_pct
+from employees
+where commission_pct is not null;
+
+-- 담당매니저가 없고 커미션 비율이 없는 직원의 이름을 출력하세요
+
+select first_name
+from employees
+where manager_id is null
+and commission_pct is null;
+
+-- order by (정렬) - 연산/출력 에너지 소모 큼
+/*  select
+    from
+    where(optional)
+    order
+*/
+select  first_name
+        ,salary
+from employees
+order by salary desc;
+-- desc : 내림차순
+
+select  first_name
+        ,salary
+from employees
+order by salary asc;
+-- asc : 오름차순 (생략은 가능하나 작성하는것을 권고)
+
+select  first_name
+        ,salary
+from employees
+where salary >= 9000
+order by salary desc;
+
+-- [예제]
+-- 부서번호를 오름차순으로 정렬하고 부서번호, 급여, 이름을 출력하세요
+
+select  department_id
+        ,salary
+        ,first_name
+from employees
+order by department_id asc;
+
+-- 급여가 10000이상인 직원의 이름 급여를 급여가 큰 직원부터 출력하세요
+
+select  first_name
+        ,salary
+from employees
+where salary >= 10000
+order by salary desc;
+
+-- 부서번호를 오름차순으로 정렬하고 부서번호가 같으면 급여가 높은 사람부터 부서번호 급여 이름을 출력하세요
+
+select  department_id
+        ,salary
+        ,first_name
+from employees
+order by department_id asc, salary desc;
+-- 복수의 정렬조건에는 ,(comma)로 구분하여 나열
+
+
+
+-- 단일행 함수
+-- 1) 문자함수
+-- INITCAP(컬럼명) - 첫 문자 대문자 출력
+select  email
+        ,initcap(email) "email2"
+        ,department_id
+from employees
+where department_id= 100;
+
+
+-- LOWER / UPPER (컬럼명) - 입력값 일괄변경_ 소문자 / 대문자
+select  first_name
+        ,lower(first_name)
+        ,upper(first_name)
+from employees
+where department_id= 100;
+
+-- SUBSTR(컬럼명, 시작위치_포함, 글자개수) - 문자열에서 특정길이 문자열 추출
+select  first_name
+        ,substr(first_name,1,3)
+        ,substr(first_name,-3,2)
+from employees
+where department_id= 100;
+
+
+-- L(left)/R(right)PAD (컬럼명, 자리수, '채우고자 하는 문자')
+-- 자리수에서 설정한 값만큼 data가 설정됨
+-- ex) first_name에서 이름의 자리수가 5개이면 채우려는 문자는 5개만큼 채워지고,
+-- 자리수가 4개일 경우 6개가 채워지는 원리
+select  first_name
+        ,lpad(first_name,10,'*')
+        ,rpad(first_name,10,'*')
+from employees;
+
+-- REPLACE (컬럼명, 찾을 문자, 바꿀 문자) - 치환
+select  first_name
+        ,replace(first_name, 'a', '*')
+from employees
+where department_id=100;
+
+select  first_name
+        ,replace(first_name, substr(first_name,2,3), '***' )
+from employees
+where department_id=100;
+
+
+-- dual (가상의 테이블) 테스트용으로 활용됨
+select substr('921024-1234565', 8, 1)
+from dual;
+
+-- 2) 숫자함수
+-- ROUND(숫자, 출력을 원하는 자릿수) - 반올림
+select  round(123.346, 2) "r2"
+        ,round(123.456, 0) "r0"
+        ,round(123.456, -1) "r-1"
+from dual;
+
+-- TRUNC(숫자, 출력을 원하는 자릿수) - 버림
+select  trunc(123.346, 2) "r2"
+        ,trunc(123.456, 0) "r0"
+        ,trunc(123.456, -1) "r-1"
+from dual;
 
 
 
