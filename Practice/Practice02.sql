c34edf5d10b35d042b584e01ced060b8cbd73b5a
Mocking_jayk0918 <jesper2able@gmail.com>
2022-05-14 23:41:36 +0900

    1.0(SQL_practice 22.05.14 23:41)

--- Practice/Practice02.sql
+++ Practice/Practice02.sql
@@ -117,11 +117,19 @@ group by salary, manager_id, hire_date
 order by avg(salary) desc;
 
 
-
-
-
-
 -- 문제10
 -- 아래회사는 보너스 지급을 위해 직원을 입사일 기준으로 나눌려고 합니다.
 -- 입사일이 02/12/31일 이전이면 '창립맴버, 03년은 '03년입사’, 04년은 ‘04년입사’ 이후입사자는 ‘상장이후입사’ optDate 컬럼의 데이터로 출력하세요.
 -- 정렬은 입사일로 오름차순으로 정렬합니다.
+
+select  first_name
+        ,hire_date
+        ,case    when hire_date <= '02/12/31' then '창립멤버'
+                when hire_date <= '03/12/31' then '03년입사'
+                when hire_date <= '04/12/31' then '04년입사'
+                else '상장 이후 입사'
+        end optDate
+from employees
+order by hire_date asc;
+
+
