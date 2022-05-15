148362f7fa6412a56448d64c9d66d974541b6df3
Mocking_jayk0918 <jesper2able@gmail.com>
2022-05-15 17:42:29 +0900

    1.0(SQL_practice 22.05.15 17:42)

--- Practice/Practice03.sql
+++ Practice/Practice03.sql
@@ -115,12 +115,36 @@ order by co.country_name asc;
 -- 과거의 업무아이디(job_id)가 ‘AC_ACCOUNT’로 근무한 사원의 사번, 이름(풀네임), 업무아이디, 시작일, 종료일을 출력하세요.
 -- 이름은 first_name과 last_name을 합쳐 출력합니다. (2건)
 
-
+select  em.employee_id "사번"
+        ,em.first_name || ' ' || em.last_name "이름(풀네임)"
+        ,jh.job_id "업무아이디"
+        ,jh.start_date "시작일"
+        ,jh.end_date "종료일"
+        
+from    job_history jh, employees em
+where   em.employee_id = jh.employee_id
+and     jh.job_id = 'AC_ACCOUNT';
 
 
 
 -- 문제8. *
--- 각 부서(department)에 대해서 부서번호(department_id), 부서이름(department_name), 매니저(manager)의 이름(first_name), 위치(locations)한 도시(city), 나라(countries)의 이름 (countries_name) 그리고 지역구분(regions)의 이름(resion_name)까지 전부 출력해 보세요. (11건)
+-- 각 부서(department)에 대해서 부서번호(department_id), 부서이름(department_name),
+-- 매니저(manager)의 이름(first_name), 위치(locations)한 도시(city),
+-- 나라(countries)의 이름(countries_name) 그리고 지역구분(regions)의 이름(resion_name)까지 전부 출력해 보세요. (11건)
+
+select  dp.department_id "부서번호"
+        ,dp.department_name "부서이름"
+        ,em.first_name "매니저 이름"
+        ,dp.location_id "위치한 도시"
+        ,co.country_id "위치한 나라"
+        ,re.region_name "위치한 지역"
+        
+from departments dp, employees em, locations lo, countries co, regions re
+
+where   em.employee_id = dp.manager_id
+and     dp.location_id = lo.location_id
+and     lo.country_id = co.country_id
+and     co.region_id = re.region_id;
 
 
 
